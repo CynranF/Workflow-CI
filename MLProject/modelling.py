0cc558fb65a6c2c886a0bb53385405bf1ae25ed4
Farhan Imannudin <144854406+CynranF@users.noreply.github.com>
2025-12-23 16:32:03 +0800

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -16,16 +16,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-mlflow.set_experiment("Obesity-CI-Basic")
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
 
-with mlflow.start_run():
-    model = RandomForestClassifier(random_state=42)
-    model.fit(X_train, y_train)
+preds = model.predict(X_test)
+acc = accuracy_score(y_test, preds)
 
-    preds = model.predict(X_test)
-    acc = accuracy_score(y_test, preds)
-
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
+mlflow.log_metric("accuracy", acc)
+mlflow.sklearn.log_model(model, "model")
 
 print("CI training selesai. Accuracy:", acc)
