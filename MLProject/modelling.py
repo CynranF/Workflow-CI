1667266fdc994aed4c8b80fdddaa30d195cf71a4
Farhan Imannudin <144854406+CynranF@users.noreply.github.com>
2025-12-23 15:34:34 +0800

    Implement Random Forest model training and logging

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,1 +1,32 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+df = pd.read_csv("obesity_preprocessed.csv")
+
+X = df.drop("Label", axis=1)
+y = df["Label"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+mlflow.set_experiment("Obesity-CI-Basic")
+
+with mlflow.start_run():
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    preds = model.predict(X_test)
+    acc = accuracy_score(y_test, preds)
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
+
+print("CI training selesai. Accuracy:", acc)
 
